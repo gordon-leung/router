ee325bc1b8b8a419aae0fdc9a7cb7a0ce5d96f5d
Gordon <gal3@sfu.ca>
2011-03-08 21:10:47 -0800

    Implemented PING. Seems to work.

--- Ethernet.c
+++ Ethernet.c
@@ -61,21 +61,23 @@ void handleEthPacket(struct sr_instance* sr,
 			if(isFrameForMe(sr, eth_hdr, iface)){
 				//TODO: handle ip datagram
 				ip_hdr = (struct ip*)(ethPacket + sizeof(struct sr_ethernet_hdr));//cast ip header
-
+/*
 				//compute checksum
 				checksum = ip_hdr->ip_sum;
 				printf("checksum original %x\n", checksum);
 				ip_hdr->ip_sum = 0; //checksum cleared
-				checksum = csum((uint16_t*)ip_hdr, 20);
+				checksum = csum((uint16_t*)ip_hdr, 4*(ip_hdr->ip_hl));
 				printf("checksum recomputed %x\n", checksum);
 				//
-				
+*/				
 				switch(ip_hdr->ip_p)
 				{
 					case (IPPROTO_ICMP):
 					{
 						printf("IP packet is of type ICMP!\n");
-						//if(icmp_reply(sr, packet, len, interface) == 0){ printf("Sent ICMP REPLY!\n"); }
+						if(icmp_reply(sr, ethPacket, len, interface) == 0){ 
+							printf("Sent ICMP REPLY!\n"); 
+						}
 						break;
 					}
 					default:
